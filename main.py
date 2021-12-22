4e5cf53f617370abf56030253951307ea5407a52
Lynn Göttgens <57255030+lynnxg@users.noreply.github.com>
2021-12-22 09:29:14 +0000

    training progress

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from _ml import MLAgent, RandomAgent, validate, plot_validation, train, save, load, train_and_plot
 from _core import is_winner, opponent, start
+import random
 
 class MyAgent(MLAgent):
     def evaluate(self, board):
@@ -11,8 +12,19 @@ class MyAgent(MLAgent):
             reward = 0
         return reward
     
+random.seed(1)
  
 my_agent = load('MyAgent_3000')
+random_agent = RandomAgent()
+ 
+train_and_plot(
+    agent=my_agent,
+    validation_agent=random_agent,
+    iterations=50,
+    trainings=100,
+    validations=1000)
+
+
 my_agent.learning = False
 
 validation_agent = RandomAgent()
