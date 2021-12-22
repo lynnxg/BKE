0f02e6b45c79af25c2200b4365a26855e63f7262
LynnG <57255030+lynnxg@users.noreply.github.com>
2021-12-22 09:11:41 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+from _ml import MLAgent, train, save, load, train_and_plot
+from _core import is_winner, opponent, start
+
+class MyAgent(MLAgent):
+    def evaluate(self, board):
+        if is_winner(board, self.symbol):
+            reward = 1
+        elif is_winner(board, opponent[self.symbol]):
+            reward = -1
+        else:
+            reward = 0
+        return reward
+    
+ 
+my_agent = MyAgent()
+start(player_x=my_agent)
